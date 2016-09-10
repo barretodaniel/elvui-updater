6987a1f417ebbed7a36bb6287ee7d2a1ebc98f3c
Daniel Barreto <dbarr018@fiu.edu>
2016-09-10 19:04:35 -0400

    Make view more responsive and allow resizing

--- main.js
+++ main.js
@@ -8,18 +8,15 @@ function createWindow() {
   // Create the browser window
   win = new BrowserWindow({
     width: 800,
-    height: 164,
+    height: 200,
     useContentSize: true,
-    resizable: false,
-    fullscreenable: false,
-    maximizable: false,
   });
 
   // Load the index.html of the app
   win.loadURL(`file://${__dirname}/index.html`);
 
   // Open dev tools
-  // win.webContents.openDevTools();
+  win.webContents.openDevTools();
 
   // Emmited when the window is closed
   win.on('closed', () => {
