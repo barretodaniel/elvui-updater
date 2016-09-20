28e409371766e887aef422a7f285a5fbde8dd31e
Daniel Barreto <dbarr018@fiu.edu>
2016-09-19 20:50:42 -0400

    remove js files

--- app/renderer.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-const core_1 = require('@angular/core');
-const app_module_1 = require('./app.module');
-core_1.enableProdMode();
-const platform = platform_browser_dynamic_1.platformBrowserDynamic();
-platform.bootstrapModule(app_module_1.AppModule);
-//# sourceMappingURL=renderer.js.map
